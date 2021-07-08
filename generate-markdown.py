61111505cecabe9551a7be9be017ecf900b7d290
Aaroh Gokhale <aaroh.gokhale123@gmail.com>
2021-07-08 21:20:35 +0530

    Created function that generates singular block for markdown.Second version

--- generate-markdown.py
+++ generate-markdown.py
@@ -1,24 +1,26 @@
-from sort import Sort
-
-s = Sort()
 
 
-def generate_block(i, file_name):  # generates a singular block for the markdown
-    element_list = s.sort(file_name)
-    with open("markdown.md", "w", encoding="utf-8") as f:
-            if element_list[1][i] == "":
-                f.write("## " + element_list[0][i] + " = NULL\n\n")
-            else:
-                f.write("## " + element_list[0][i] + " = " + element_list[1][i] + "\n\n")
-            if element_list[1][i] == "":
-                f.write("|NULL|\n")
-            else:
-                f.write("|" + element_list[3][i] + "|\n")
-            f.write("|---|\n")
-            if element_list[1][i] == "":
-                f.write("|NULL|\n\n")
-            else:
-                f.write("|" + element_list[4][i] + "|\n\n")
-            f.write("---\n")
+def generate_block(row):  # generates a singular block for the markdown. row is a string parameter.
+    word_block = ""
+    element_list = row.split(",")
+    if element_list[1] == "":
+        word_block += "## " + element_list[0] + " = NULL\n\n"
+    else:
+        word_block += "## " + element_list[0] + " = " + element_list[1] + "\n\n"
 
+    if element_list[3] == "":
+        word_block += "|NULL|\n"
+    else:
+        word_block += "|" + element_list[3] + "|\n"
+    word_block += "|---|\n"
+    if element_list[4] == "":
+        word_block += "|NULL|\n\n"
+    else:
+        word_block += "|" + element_list[4] + "|\n\n"
+    word_block += "---\n"
+
+    return word_block
+
+
+print(generate_block("experience,अनुभव,daily,I have experience in this.,मला यात अनुभव आहे."))
 
