518cc1814a25833dee29794bb9874166fe6b818b
Aaroh Gokhale <aaroh.gokhale123@gmail.com>
2021-07-08 20:35:00 +0530

    Created function that generates singular block for markdown

--- generate-markdown.py
+++ generate-markdown.py
@@ -2,17 +2,23 @@ from sort import Sort
 
 s = Sort()
 
-element_list = s.sort("db.csv")
 
-with open("markdown.md", "w", encoding="utf-8") as f:
-    for i in range(0, len(element_list[0])):
-        if element_list[1][i] == "":
-            f.write("## " + element_list[0][i] + " = mr\n\n")
-        else:
-            f.write("## " + element_list[0][i] + " = " + element_list[1][i] + "\n\n")
-        f.write("|" + element_list[3][i] + "|\n")
-        f.write("|---|\n")
-        f.write("|" + element_list[4][i] + "|\n\n")
-        f.write("---\n")
+def generate_block(i, file_name):  # generates a singular block for the markdown
+    element_list = s.sort(file_name)
+    with open("markdown.md", "w", encoding="utf-8") as f:
+            if element_list[1][i] == "":
+                f.write("## " + element_list[0][i] + " = NULL\n\n")
+            else:
+                f.write("## " + element_list[0][i] + " = " + element_list[1][i] + "\n\n")
+            if element_list[1][i] == "":
+                f.write("|NULL|\n")
+            else:
+                f.write("|" + element_list[3][i] + "|\n")
+            f.write("|---|\n")
+            if element_list[1][i] == "":
+                f.write("|NULL|\n\n")
+            else:
+                f.write("|" + element_list[4][i] + "|\n\n")
+            f.write("---\n")
 
 
