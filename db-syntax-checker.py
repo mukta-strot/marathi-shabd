8dc90f7146e12b0d4f912cdaaddbd414c29860fb
Aaroh Gokhale <aaroh.gokhale123@gmail.com>
2021-07-10 22:23:22 +0530

    Minor correction in the syntax checker. Note - this commit doesn't address the other suggested corrections and only fixes an unanticipated issue. The code wasn't even working before this correction.

--- db-syntax-checker.py
+++ db-syntax-checker.py
@@ -4,10 +4,11 @@ import csv
 def check_syntax(file):
     with open(file, encoding="utf-8"):
         csv_reader = csv.reader(file, delimiter=",")
-        num_cols = len(csv_reader[0])
+        num_cols = len(list(csv_reader)[0])
 
         for row in csv_reader:
             if len(row) != num_cols:
                 return "error"
 
         return "no error"
+
