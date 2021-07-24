4a96faa331530d3cb29cf03264197401255e6679
Mason Woodford <mwoodfor@ucsc.edu>
2021-07-23 22:01:46 -0700

    added a topics parser class

---
+++ browse/scripts/topicsParser.py
@@ -0,0 +1,29 @@
+import csv
+
+class TopicsParser:
+    
+    def gen_topics(self, file):
+        with open(file, encoding="utf-8") as csv_file:
+            topics = set()
+            tags_column_index = 0
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            tags_column_index = self.find_tags_column_index(csv_reader)
+            for row in csv_reader:
+                split_topics = row[tags_column_index].split(';')
+                for temp_topic in split_topics:
+                    if (temp_topic == ''):
+                        pass
+                    else:
+                        topics.add(temp_topic)
+            return sorted(list(topics))
+
+    def find_tags_column_index(self, csv_reader):
+        for row in csv_reader:
+            tags_column_index = 0
+            for column in row:
+                if (column == 'tags'):
+                    return tags_column_index
+                tags_column_index += 1
+
+            
+            
