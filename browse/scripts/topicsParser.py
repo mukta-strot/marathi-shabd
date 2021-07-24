32861d128344c78c3fe19e43c28e7d4c68d0d9e5
Mason Woodford <mwoodfor@ucsc.edu>
2021-07-23 23:07:20 -0700

    refactoring/tests of topics parser

--- browse/scripts/topicsParser.py
+++ browse/scripts/topicsParser.py
@@ -6,7 +6,7 @@ class TopicsParser:
         with open(file, encoding="utf-8") as csv_file:
             topics = set()
             tags_column_index = 0
-            csv_reader = csv.reader(csv_file, delimiter=',')
+            csv_reader = list(csv.reader(csv_file, delimiter=','))
             tags_column_index = self.find_tags_column_index(csv_reader)
             for row in csv_reader:
                 split_topics = row[tags_column_index].split(';')
@@ -18,12 +18,16 @@ class TopicsParser:
             return sorted(list(topics))
 
     def find_tags_column_index(self, csv_reader):
-        for row in csv_reader:
-            tags_column_index = 0
-            for column in row:
-                if (column == 'tags'):
-                    return tags_column_index
-                tags_column_index += 1
+        tags_column_index = 0
+        for column in csv_reader[0]:
+            if (column == 'tags'):
+                return tags_column_index
+            tags_column_index += 1
 
-            
+
+#TESTS
+
+#t = TopicsParser()
+
+#t.gen_topics("../../database/db.csv")
             
