98d999227b627b341eeba7ff426ecaad89067e45
Sahil Gupta <guptasahil670@gmail.com>
2023-12-27 15:47:40 +0000

    Added Search By Tags Feature

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,10 @@
 const csvData = []
+const uniqueTags = new Set()
 
 // Fetching the HTML DOM elements
 const results = document.querySelector('#results-data')
 const wordInput = document.querySelector('#query_english')
+const tagInput = document.querySelector("#search-box-tag")
 
 // Handler function for search button
 function searchHandler() {
@@ -13,7 +15,7 @@ function searchHandler() {
 
   // Remove leading and trailing whitespaces 
   searchKeyword = searchKeyword.trim()
-  
+
   // Search for the keyword from the array
   let matchedResults = csvData.filter(row => {
     return row.en == searchKeyword
@@ -50,7 +52,7 @@ function searchHandler() {
 
 // Preload - load all the csv data into an array
 async function fetchDataCSV() {
-  
+
   // fetch db.csv 
   fetch('../database/db.csv', {mode: 'no-cors'})
     .then(resp => resp.text())
@@ -73,8 +75,86 @@ async function fetchDataCSV() {
         })
       }
 
+      getUniqueTags();    // get all unique tags in the data
+      populateSearch();   // populate the searchlist using above tags
     })
     .catch(err => console.log(err))
 }
 
 fetchDataCSV()
+
+// get all unique tags from the data
+function getUniqueTags() {
+  for (let entry of csvData) {
+    if (typeof (entry['tags']) !== 'undefined') {
+      entry['tags'].split(';').forEach(tag => {
+        uniqueTags.add(tag);
+      });
+    }
+  }
+  uniqueTags.delete(''); // remove empty tags
+  uniqueTags.delete('tags'); // remove the header from csv
+}
+
+
+// populate the search box dropdown with tags
+function populateSearch() {
+  const searchItems = document.getElementById("search-items");
+  for (const tag of uniqueTags) {
+    // create a list-element for every tag found
+    const item = document.createElement("option");
+    item.setAttribute("value", tag);
+    searchItems.appendChild(item);
+  }
+}
+
+// search handler for searching via tags
+function searchByTag() {
+  tagInput.blur();    // remove focus from input
+  const searchQuery = tagInput.value;
+
+  // if search query is not a tag then return
+  if (!uniqueTags.has(searchQuery)) {
+    results.innerHTML = "Select an appropriate tag to filter words"
+    return
+  }
+
+  // FIlter the words with matching tags in the data
+  let matchedResults = csvData.filter(row => {
+    let t = row.tags;
+    return typeof (t) === 'string' && t.includes(searchQuery);
+  })
+
+  if (matchedResults.length === 0) {
+    results.innerHTML = "Currently no words are assigned to this tag"
+    return
+  }
+
+  // display results in results section
+  // create a list to wrap all the results
+  results.innerHTML = '<ul type="none" style="padding: 0;"></ul>';
+
+  for (const item of matchedResults) {
+    // create a list item for every matched result
+    const resultItem = document.createElement("li");
+    resultItem.innerHTML = "<span>";
+    resultItem.innerHTML += "<b>" + item.en.toUpperCase() + "</b> : ";
+    resultItem.innerHTML += item.mr + "<br>";
+
+    if (item.en_ex)
+      resultItem.innerHTML += "<b>English example: </b>" + item.en_ex + "<br>";
+    else
+      resultItem.innerHTML += "English example currently not available for this word<br>";
+
+    if (item.mr_ex)
+      resultItem.innerHTML += "<b>Marathi example: </b>" + item.mr_ex + "<br>";
+    else
+      resultItem.innerHTML += "Marathi example currently not available for this word<br>"
+
+    resultItem.innerHTML += "</span><br>";
+
+    // append the resultItem to result list
+    document.querySelector("#results-data > ul").appendChild(resultItem);
+  }
+  return
+}
